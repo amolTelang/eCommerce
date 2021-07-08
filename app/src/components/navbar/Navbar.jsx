63838a884a50364becde019f8a1c0f14336de88b
Amol Telang <telangamol0909@gmail.com>
2021-07-08 14:56:23 +0530

    started commerce js template

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { AppBar,Toolbar,IconButton,Badge,MenuItem,Menu,Typography } from '@material-ui/core'
+import React,{Fragment} from 'react'
+import { AppBar,Toolbar,IconButton,Badge,MenuItem,Menu,Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assests/shopIcon.png';
+import useStyles from './styles';
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
+    const classes=useStyles();
+        return (
+       <Fragment>
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+                <Typography variant="h6" className={classes.title} color="inherit">
+                    <img src={logo} alt="Commerce" height="25px" className={classes.image}/>
+                    Commerce
+                </Typography>
+                <div className={classes.grow}/>
+                <div className={classes.button}/>
+                    <IconButton aria-label="Show cart items" color="inherit">
+                        <Badge badgeContent={2} color="secondary">
+                            <ShoppingCart/>
+                        </Badge>
+                    </IconButton>
+            </Toolbar>
+        </AppBar>
+       </Fragment>
     )
 }
 
