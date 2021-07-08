eefe33d305168798e07eaec270ba94d9e47133d4
Amol Telang <telangamol0909@gmail.com>
2021-07-08 12:19:44 +0530

    created Products and Product component

---
+++ app/src/components/products/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card,CardMedia,CardContent,CardActions,Typography,IconsBotton } from '@material-ui/core';
+import {AddShoppingCart} from '@material-ui/icons'
+const Product = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Product
